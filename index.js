1129645aa9e10e1e3316406f0d3e70b2dd16fc86
DusekDan <dusekdan@gmail.com>
2018-11-30 22:43:38 +0300

    Update - Minor tweaks and improvements

--- index.js
+++ index.js
@@ -4,45 +4,66 @@ const log = console.log;
 // Layers of the (play) screen
 const farBackground = new PIXI.Container();
 const closeBackground = new PIXI.Container();
-farBackground.zIndex = 1;
-closeBackground.zIndex = 2;
 
-app.stage.addChild(farBackground);
-app.stage.addChild(closeBackground);
 
 // Managers interacting with the game.
 var cloudManager;
 var wallManager;
+var screenManager;
 
 const init = () => {
+    log ("Game loaded.");
     
-    log ("Resources successfully loaded. Starting the game.");
-    
-    // Check that document object model exists before trying to alter it.
     if (document) {
         document.getElementById('the-play-screen').appendChild(app.view);
     } else {
-        log ("Error: The Document Object Model does not exist. It was not possible to create player view.");
+        log ("Error: DOM does not exist, unable to load the game.");
+        alert("It was not possible to load the game.");
     }
+
+    // Instantiating screen manager prepares all the screens and puts the 
+    // mainscreen into the foreground. Instantiation must be done within
+    // start function, which is called AFTER all the game resources are
+    // loaded. This way availability of textures/sprites/globals is ensured.
+    screenManager = new ScreenManager();
+}
+
+const startGame = () => {
     
     // Instantiate necessary managers.
     cloudManager = new CloudManager(app, farBackground);
     wallManager = new WallManager(app, closeBackground);
 
- 
+    // Configure background layers and add them to the stage.
+    farBackground.zIndex = 1;
+    closeBackground.zIndex = 2;
+    app.stage.addChild(farBackground);
+    app.stage.addChild(closeBackground);
 
     // Set up the basics for rendered canvas.
     app.renderer.backgroundColor = 0x22a7f0;
     app.renderer.render(app.stage);
+    
     gameLoop();
+    // Try ticker approach instead of gameLoop()
+    //app.ticker.add(tickerCallback);
 }
 
 const gameLoop = () => {
     requestAnimationFrame(gameLoop);
-    cloudManager.update();
+    cloudManager.runTick();
     wallManager.runTick();
 };
 
+const tickerCallback = deltaTime => {
+    requestAnimationFrame();
+    cloudManager.runTick(deltaTime);
+    log("Ticker callback called.");
+};
+
+
+//var ticker = new PIXI.ticker.Ticker();
+//app.ticker.add(() => {log("Ticker: " + ticker.deltaTime);});
 
 // Assets loading call has to be executed after the init function is defined.
 loadPIXIResources(init);
--- utils.js
+++ utils.js
@@ -8,4 +8,12 @@ class Utils {
     static randomIntColor () {
         return parseInt(Math.floor(Math.random()*16777215).toString(16), 16)
     }
+
+    static createSpriteFromGraphics(renderer, graphics) {
+        let texture = PIXI.RenderTexture.create(
+            graphics.width, graphics.height);
+        
+        renderer.render(graphics, texture);
+        return new PIXI.Sprite(texture);
+    }
 }
