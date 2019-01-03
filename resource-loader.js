bceacd96f86d890e67a8fd362d70139e62051aef
DusekDan <dusekdan@gmail.com>
2019-01-03 21:53:55 +0300

    Update - resource loader script now loads desert biom resources

--- resource-loader.js
+++ resource-loader.js
@@ -6,7 +6,24 @@ const resources = [
     imagesFolder + "cloud_2.png",
     imagesFolder + "entity_place_holder.png",
     imagesFolder + "lock-50.png",
-    imagesFolder + "heart-icon.png"
+    imagesFolder + "heart-icon.png",
+    imagesFolder + "pyramid_01_huge.png",
+    imagesFolder + "pyramid_01_medium.png",
+    imagesFolder + "pyramid_01_ultrahuge.png",
+    imagesFolder + "pyramid_01_ultrasmall.png",
+    imagesFolder + "pyramid_01_small.png",
+    imagesFolder + "sandblock_01_huge.png",
+    imagesFolder + "sandblock_01_medium.png",
+    imagesFolder + "sandblock_01_small.png",
+    imagesFolder + "sandblock_01_ultrasmall.png",
+    imagesFolder + "sandwall_01_huge.png",
+    imagesFolder + "sandwall_01_medium.png",
+    imagesFolder + "sandwall_01_small.png",
+    imagesFolder + "sandwall_01_ultrasmall.png",
+    imagesFolder + "sandwall_02_huge.png",
+    imagesFolder + "sandwall_02_medium.png",
+    imagesFolder + "sandwall_02_small.png",
+    imagesFolder + "sandwall_02_ultrasmall.png"
 ]
 
 const loadPIXIResources = loadedCallback => {
