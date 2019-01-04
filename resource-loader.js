f89ed035284016f564cff88eff8243e333899c51
DusekDan <dusekdan@gmail.com>
2019-01-04 10:57:29 +0300

    Add - Winter biom resource files and loading

--- resource-loader.js
+++ resource-loader.js
@@ -1,6 +1,7 @@
 const assetsFolder = "assets/";
 const imagesFolder = assetsFolder + "images/";
-const desertBiomFolder = imagesFolder + "BIOM_DESERT/"
+const desertBiomeFolder = imagesFolder + "BIOM_DESERT/";
+const winterBiomeFolder = imagesFolder + "BIOME_WINTER/";
 
 const resources = [
     imagesFolder + "cloud_1.png",
@@ -8,23 +9,37 @@ const resources = [
     imagesFolder + "entity_place_holder.png",
     imagesFolder + "lock-50.png",
     imagesFolder + "heart-icon.png",
-    desertBiomFolder + "pyramid_01_huge.png",
-    desertBiomFolder + "pyramid_01_medium.png",
-    desertBiomFolder + "pyramid_01_ultrahuge.png",
-    desertBiomFolder + "pyramid_01_ultrasmall.png",
-    desertBiomFolder + "pyramid_01_small.png",
-    desertBiomFolder + "sandblock_01_huge.png",
-    desertBiomFolder + "sandblock_01_medium.png",
-    desertBiomFolder + "sandblock_01_small.png",
-    desertBiomFolder + "sandblock_01_ultrasmall.png",
-    desertBiomFolder + "sandwall_01_huge.png",
-    desertBiomFolder + "sandwall_01_medium.png",
-    desertBiomFolder + "sandwall_01_small.png",
-    desertBiomFolder + "sandwall_01_ultrasmall.png",
-    desertBiomFolder + "sandwall_02_huge.png",
-    desertBiomFolder + "sandwall_02_medium.png",
-    desertBiomFolder + "sandwall_02_small.png",
-    desertBiomFolder + "sandwall_02_ultrasmall.png"
+    desertBiomeFolder + "pyramid_01_huge.png",
+    desertBiomeFolder + "pyramid_01_medium.png",
+    desertBiomeFolder + "pyramid_01_ultrahuge.png",
+    desertBiomeFolder + "pyramid_01_ultrasmall.png",
+    desertBiomeFolder + "pyramid_01_small.png",
+    desertBiomeFolder + "sandblock_01_huge.png",
+    desertBiomeFolder + "sandblock_01_medium.png",
+    desertBiomeFolder + "sandblock_01_small.png",
+    desertBiomeFolder + "sandblock_01_ultrasmall.png",
+    desertBiomeFolder + "sandwall_01_huge.png",
+    desertBiomeFolder + "sandwall_01_medium.png",
+    desertBiomeFolder + "sandwall_01_small.png",
+    desertBiomeFolder + "sandwall_01_ultrasmall.png",
+    desertBiomeFolder + "sandwall_02_huge.png",
+    desertBiomeFolder + "sandwall_02_medium.png",
+    desertBiomeFolder + "sandwall_02_small.png",
+    desertBiomeFolder + "sandwall_02_ultrasmall.png",
+    winterBiomeFolder + "angry_snowman_huge.png",
+    winterBiomeFolder + "angry_snowman_ultrasmall.png",
+    winterBiomeFolder + "dead_snowman_full_huge.png",
+    winterBiomeFolder + "dead_snowman_full_ultrasmall.png",
+    winterBiomeFolder + "dead_snowman_head_medium.png",
+    winterBiomeFolder + "dead_snowman_head_small.png",
+    winterBiomeFolder + "dead_snowman_head_ultrasmall.png",
+    winterBiomeFolder + "snowball_pack_01_medium.png",
+    winterBiomeFolder + "snowball_pack_01_small.png",
+    winterBiomeFolder + "snowball_pack_01_ultrasmall.png",
+    winterBiomeFolder + "snowball_pack_02_medium.png",
+    winterBiomeFolder + "snowball_pack_02_small.png",
+    winterBiomeFolder + "snowball_pack_02_ultrasmall.png",
+    winterBiomeFolder + "snowball_small.png"
 ]
 
 const loadPIXIResources = loadedCallback => {
