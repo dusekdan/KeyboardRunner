a2542d4a8cfc9cabd3ff1fa03066ae2eb45b626a
DusekDan <dusekdan@gmail.com>
2019-01-04 08:02:15 +0300

    Update - Relocate desert biome resource file

--- resource-loader.js
+++ resource-loader.js
@@ -1,5 +1,6 @@
 const assetsFolder = "assets/";
 const imagesFolder = assetsFolder + "images/";
+const desertBiomFolder = imagesFolder + "BIOM_DESERT/"
 
 const resources = [
     imagesFolder + "cloud_1.png",
@@ -7,23 +8,23 @@ const resources = [
     imagesFolder + "entity_place_holder.png",
     imagesFolder + "lock-50.png",
     imagesFolder + "heart-icon.png",
-    imagesFolder + "pyramid_01_huge.png",
-    imagesFolder + "pyramid_01_medium.png",
-    imagesFolder + "pyramid_01_ultrahuge.png",
-    imagesFolder + "pyramid_01_ultrasmall.png",
-    imagesFolder + "pyramid_01_small.png",
-    imagesFolder + "sandblock_01_huge.png",
-    imagesFolder + "sandblock_01_medium.png",
-    imagesFolder + "sandblock_01_small.png",
-    imagesFolder + "sandblock_01_ultrasmall.png",
-    imagesFolder + "sandwall_01_huge.png",
-    imagesFolder + "sandwall_01_medium.png",
-    imagesFolder + "sandwall_01_small.png",
-    imagesFolder + "sandwall_01_ultrasmall.png",
-    imagesFolder + "sandwall_02_huge.png",
-    imagesFolder + "sandwall_02_medium.png",
-    imagesFolder + "sandwall_02_small.png",
-    imagesFolder + "sandwall_02_ultrasmall.png"
+    desertBiomFolder + "pyramid_01_huge.png",
+    desertBiomFolder + "pyramid_01_medium.png",
+    desertBiomFolder + "pyramid_01_ultrahuge.png",
+    desertBiomFolder + "pyramid_01_ultrasmall.png",
+    desertBiomFolder + "pyramid_01_small.png",
+    desertBiomFolder + "sandblock_01_huge.png",
+    desertBiomFolder + "sandblock_01_medium.png",
+    desertBiomFolder + "sandblock_01_small.png",
+    desertBiomFolder + "sandblock_01_ultrasmall.png",
+    desertBiomFolder + "sandwall_01_huge.png",
+    desertBiomFolder + "sandwall_01_medium.png",
+    desertBiomFolder + "sandwall_01_small.png",
+    desertBiomFolder + "sandwall_01_ultrasmall.png",
+    desertBiomFolder + "sandwall_02_huge.png",
+    desertBiomFolder + "sandwall_02_medium.png",
+    desertBiomFolder + "sandwall_02_small.png",
+    desertBiomFolder + "sandwall_02_ultrasmall.png"
 ]
 
 const loadPIXIResources = loadedCallback => {
